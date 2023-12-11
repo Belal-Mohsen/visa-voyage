cee3897e963622de63d0b9e87baba0bf622bb7c7
Belal-Mohsen <belal4mohsen@gmail.com>
2023-12-11 11:12:42 -0500

    Added the hero component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
         setNav(!nav);
     }
     return (
-        <div className='flex justify-between items-center h-20 px-4'>
+        <div className='flex w-full justify-between items-center h-20 px-4 absolute z-10 text-white'>
             <div>
                 <h1 className='logo'>
                     VISA VOYAGE</h1>
@@ -28,14 +28,14 @@ const Navbar = () => {
                 <BsPerson size={25} />
             </div>
             <div className='md:hidden'>
-            <div onClick={handleNav}>
+            <div onClick={handleNav} className='z-10'>
                 {
-                    nav ? <AiOutlineClose className='text-black z-10' size={20} /> : <HiOutlineMenuAlt4 size={20} />
+                    nav ? <AiOutlineClose className='text-black' size={20} /> : <HiOutlineMenuAlt4 size={20} />
                 }
             </div>
 
 
-            <div onClick={handleNav} className={nav ? 'absolute left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col' : 'absolute left-[-100%]'}>
+            <div onClick={handleNav} className={nav ? 'absolute text-black left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col' : 'absolute left-[-100%]'}>
                 <ul>
                     <h1 className='logo'> VISA VOYAGE</h1>
                     <li className='border-b'>Home</li>
